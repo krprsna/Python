4f475c86de5361ac54892841b045574e2db2de9a
krprsna <31450306+krprsna@users.noreply.github.com>
2026-02-24 15:58:14 +0530

    Update 12-Dic_Nesting.py

--- 100DayOfPython/Beginners/12-Dic_Nesting.py
+++ 100DayOfPython/Beginners/12-Dic_Nesting.py
@@ -29,11 +29,8 @@ Loop
 
 # Using for loop with dic
 for i in programming_dictionary:
-    print(programming_dictionary[i]) # Prints both the key:value
-Bug
+    print(programming_dictionary[i]) # Prints only the value
 An error in a program that prevents the program from running as expected.
-Function
 A piece of code that you can easily call over and over again.
-Loop
 Action that is executed again and again
 
