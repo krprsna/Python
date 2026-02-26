d271879dc5cca48de8e6f6230e2f69d6dad7f717
krprsna <31450306+krprsna@users.noreply.github.com>
2026-02-26 15:00:56 +0530

    Update 14-Nesting..py

--- 100DayOfPython/Beginners/14-Nesting..py
+++ 100DayOfPython/Beginners/14-Nesting..py
@@ -33,4 +33,12 @@ travel_log = {
     "total_visits": 5
    },
 }
+print(travel_logs)
+{'France': {'cities_visited': ['Paris', 'Lille', 'Dijon'], 'total_visits': 12}, 'Germany': {'cities_visited': ['Berlin', 'Hamburg', 'Stuttgart'], 'total_visits': 5}}
+
+print(travel_logs["Germany"]["cities_visited"])
+['Berlin', 'Hamburg', 'Stuttgart']
+
+print(travel_logs["Germany"]["cities_visited"][2])
+Stuttgart
 
