beb00323ba79b7ab7309c5ef8b9844f7c7955cd0
krprsna <31450306+krprsna@users.noreply.github.com>
2026-02-26 14:58:12 +0530

    Update 14-Nesting..py

--- 100DayOfPython/Beginners/14-Nesting..py
+++ 100DayOfPython/Beginners/14-Nesting..py
@@ -1,9 +1,8 @@
-# Dic wiht List
+# Dic with List
 travel_log = {
     "France": ["Paris", "Lille", "Dijon"],
     "Germany": ["Stuttgart", "Berlin"],
 }
-
 print(travel_log)
 {'France': ['Paris', 'Lille', 'Dijon'], 'Germany': ['Stuttgart', 'Berlin']}
 
@@ -13,6 +12,7 @@ print(travel_log["France"])
 print(travel_log["France"][1])
 Lille
 
+
 # List with List
 nested_list = ["A", "B", ["C", "D"]]
 print(nested_list)
@@ -20,3 +20,17 @@ print(nested_list)
 
 print(nested_list[2][1])
 D
+
+
+# Dict with dict
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+
