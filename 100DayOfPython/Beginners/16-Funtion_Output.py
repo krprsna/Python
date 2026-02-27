a4832f153969b85146512c414f0d3224db563fca
krprsna <31450306+krprsna@users.noreply.github.com>
2026-02-27 22:01:20 +0530

    Update 16-Funtion_Output.py

--- 100DayOfPython/Beginners/16-Funtion_Output.py
+++ 100DayOfPython/Beginners/16-Funtion_Output.py
@@ -5,3 +5,25 @@ print(format_name("prasanna", "kr"))
 Prasanna Kr
 
 
+# Indivudual function does two different operation 
+def func_1(text):
+    return text + text
+print(func_1("prasanna"))
+prasannaprasanna
+
+def func_2(text):
+    return text.title()
+print(func_2("prasanna"))
+Prasanna
+
+
+# Output of a function is used as input for the another fumction
+def func_1(text):
+    return text + text
+def func_2(text):
+    return text.title()
+print(func_1(func_2("prasanna"))) 
+PrasannaPrasanna
+
+print(func_2(func_1("prasanna")))
+Prasannaprasanna
