ec74fdea65b8e3ccec993792c37cb4783601fb04
krprsna <31450306+krprsna@users.noreply.github.com>
2026-03-07 22:12:51 +0530

    Update 21-guess_the_number.py

--- 100DayOfPython/Beginners/21-guess_the_number.py
+++ 100DayOfPython/Beginners/21-guess_the_number.py
@@ -1,6 +1,6 @@
 # Method 1
-import random
-number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"]
+from random import randint
+number = randint(1, 100)
 random_num = random.choice(number)
 print(random_num)
 
@@ -8,7 +8,7 @@ def hard():
     life = 5
     game_over = True
     while game_over:
-        get_user_input = input("\nEnter a number to guess: ")
+        get_user_input = int(input("\nEnter a number to guess: "))
         if get_user_input == random_num:
             print("Correct!")
             game_over = False
@@ -30,7 +30,7 @@ def easy():
     life = 10
     game_over = True
     while game_over:
-        get_user_input = input("\nEnter a number to guess: ")
+        get_user_input = int(input("\nEnter a number to guess: "))
         if get_user_input == random_num:
             print("Correct!")
             game_over = False
